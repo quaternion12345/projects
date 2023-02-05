3f4cde7be5908c9c4b6807f03ccd0d3d64298518
fermion <fermion>
2023-02-05 21:13:26 +0900

    Mustache

--- springboot-webservice/src/main/java/com/example/book/springboot/service/PostsService.java
+++ springboot-webservice/src/main/java/com/example/book/springboot/service/PostsService.java
@@ -2,13 +2,17 @@ package com.example.book.springboot.service;
 
 import com.example.book.springboot.domain.posts.Posts;
 import com.example.book.springboot.domain.posts.PostsRepository;
+import com.example.book.springboot.web.dto.PostsListResponseDto;
 import com.example.book.springboot.web.dto.PostsResponseDto;
 import com.example.book.springboot.web.dto.PostsSaveRequestDto;
 import com.example.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,22 @@ public class PostsService {
                         () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
                 );
         return new PostsResponseDto(entity);
+    }
+
+    // 범위는 유지하되, 조회 기능만 남겨두어 조회 속도를 개선
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // posts stream을 PostsListResponseDto로 변환
+                .collect(Collectors.toList());  // List로 변환
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+
+        postsRepository.delete(posts);
     }
 }
